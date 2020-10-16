71e1850e50e893e6552948d9e6f44ec3bbca53d9
Blake Videbeck <blakevidebeck@gmail.com>
2020-10-16 15:50:13 +1300

    initial commit

---
+++ client/build/precache-manifest.1474ac37f05c2610c19e3a77985d7c8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1039f630e62c7a1bb119c7e0a22987a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0935fa658575174aad7",
+    "url": "/static/css/main.842e7c3b.chunk.css"
+  },
+  {
+    "revision": "6db62c1a07d8f93bdccd",
+    "url": "/static/js/2.173b4836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.173b4836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0935fa658575174aad7",
+    "url": "/static/js/main.14329dcc.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
